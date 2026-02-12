20ed82ff0ea4cb2a6042405ca6e9be97bef7c237
RamiMaroun <s351882@oslomet.no>
2026-02-12 06:42:17 +0100

    make it reponisve

--- code.js
+++ code.js
@@ -6,3 +6,25 @@ setTimeout(function () {
   }
 
 
+const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.close');
+const links = document.querySelector('.sidebar-links');
+hamburger.addEventListener('click', function() {
+  hamburger.style.display = 'none';
+  close.style.display = 'block';
+ links.classList.add('active-sidebar');
+});
+close.addEventListener('click', function() {
+  hamburger.style.display = 'block';
+  links.classList.remove('active-sidebar');
+  close.style.display = 'none';
+});
+window.addEventListener('resize', () => {
+  // 👉 Run this code every time the browser window changes size
+
+  if (window.innerWidth > 1100) {
+    hamburger.style.display = '';   // رجّع التحكم للـ CSS
+    close.style.display = '';       // رجّع التحكم للـ CSS
+    links.classList.remove('active-sidebar'); // سكّر القائمة
+  }
+});
