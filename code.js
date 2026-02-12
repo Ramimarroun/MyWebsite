69d50a4ddf3f037d76b1b62f0e979d937f760b43
RamiMaroun <s351882@oslomet.no>
2026-02-12 01:56:20 +0100

    Add alert

---
+++ code.js
@@ -0,0 +1,8 @@
+setTimeout(function () {
+    document.getElementById("customAlert").style.display = "block";
+  }, 2000);
+  function closeAlert() {
+    document.getElementById("customAlert").style.display = "none";
+  }
+
+
